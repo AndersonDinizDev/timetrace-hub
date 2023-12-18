a6a3187470bbeab8d637341ae7dbba082b54981a
andersondinizdev <andersondiniz159@gmail.com>
2023-12-18 09:11:38 -0300

    Relizando testes de consulta.

--- src/models/Model.php
+++ src/models/Model.php
@@ -29,26 +29,36 @@ class Model
     $this->values[$key] = $value;
   }
 
-  public static function getSelect($filters = [], $columns = '*') {
+  public static function getResultSetFromSelect($filters = [], $columns = '*')
+  {
     $sql = "SELECT $columns FROM " . static::$tableName . static::getFilters($filters);
-    return $sql;
+    $result = Database::getResultFromQuery($sql);
+
+    if ($result !== false && !empty($result)) {
+
+      return $result;
+    } else {
+      return null;
+    }
   }
 
-  private static function getFilters($filters) {
+  private static function getFilters($filters)
+  {
     $sql = '';
-    if(count($filters) > 0) {
+    if (count($filters) > 0) {
       $sql .= " WHERE 1 = 1";
-      foreach($filters as $column => $value) {
+      foreach ($filters as $column => $value) {
         $sql .= " AND $column = " . static::getFormatedValue($value);
       }
     }
     return $sql;
   }
 
-  private static function getFormatedValue($value) {
-    if(is_null($value)) {
+  private static function getFormatedValue($value)
+  {
+    if (is_null($value)) {
       return "null";
-    } elseif(gettype($value) === 'string') {
+    } elseif (gettype($value) === 'string') {
       return "'$value'";
     } else {
       return $value;
--- www/index.php
+++ www/index.php
@@ -5,6 +5,6 @@ require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
 $user = new User(['name' => 'teste', 'email' => 'teste@teste.com']);
 
-echo User::getSelect(['id' => '1'], 'name, email');
-echo "<br/>";
-echo User::getSelect(['name' => 'chaves', 'email' => 'teste@novoteste.com']);
+// echo User::getSelect(['id' => '1'], 'name, email');
+// echo "<br/>";
+// echo User::getSelect(['name' => 'chaves', 'email' => 'teste@novoteste.com']);
