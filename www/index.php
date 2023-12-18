078d2e5797e454ecc37ff19748665fce154ca4e5
andersondinizdev <andersondiniz159@gmail.com>
2023-12-18 08:38:02 -0300

    Realizando alguns testes.

--- www/index.php
+++ www/index.php
@@ -6,3 +6,6 @@ require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 $user = new User(['name' => 'teste', 'email' => 'teste@teste.com']);
 
 print_r($user);
+echo '<br/><br/>';
+$user->email = 'teste@novo.com';
+print_r($user->email);
